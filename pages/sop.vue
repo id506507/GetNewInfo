e1f998f27014732bdd829af83c231371b5915841
Angus Chan <id506507@gmail.com>
2019-07-25 17:23:53 +0800

    edit proxy

--- pages/sop.vue
+++ pages/sop.vue
@@ -12,10 +12,9 @@ import axios from 'axios';
 @Component
 export default class UsefulPage extends Vue{
     async asyncData(){
-        const proxyurl='https://cors-anywhere.herokuapp.com/';
+        const proxyurl='https://test.cors.workers.dev/?';
         let [waiting]=await Promise.all([
-            // axios.get('https://www.ha.org.hk/opendata/sop/sop-waiting-time-tc.json'),
-            axios.get('https://yacdn.org/proxy/http://www.ha.org.hk/opendata/sop/sop-waiting-time-tc.json')
+            axios.get(proxyurl+'https://www.ha.org.hk/opendata/sop/sop-waiting-time-tc.json'),
         ])
         return {
             Waiting:waiting.data,
