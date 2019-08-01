a472a042dda0b903777aec540d871ca1328b7ca6
Angus Chan <id506507@gmail.com>
2019-08-01 14:09:32 +0800

    update weather_detail page

---
+++ pages/weather_forecast.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container">
+        <div>
+            <p class="title">天氣預報</p>
+            <p class="subtitle">一般天氣預測</p>
+            <p class="cards">{{Flws.generalSituation}}{{Flws.tcInfo}}{{Flws.fireDangerWarning}}</p>
+            <p class="cards">{{Flws.forecastPeriod}}：{{Flws.forecastDesc}}{{Flws.outlook}}</p>
+            <p class="time">更新時間：{{$moment(Flws.updateTime).format('YYYY-MM-DD LTS')}}</p>
+            <p class="subtitle">九天天氣預報</p>
+            <div>{{Nine.generalSituation}}</div>
+            <div>
+                <b-card-group>
+                    <div>
+                        <b-card style="width:100%" v-for="(item,index) in Nine.weatherForecast" :key="index">
+                            <b-card-sub-title>
+                                {{$moment(item.forecastDate).format('YYYY-MM-DD')}}&nbsp;({{item.week}})
+                            </b-card-sub-title>
+                            <br>
+                            <b-card-text>
+                                {{item.forecastWeather}}{{item.forecastWind}}
+                            </b-card-text>
+                            <b-card-text>溫度：{{item.forecastMintemp.value}}&#176;{{item.forecastMintemp.unit}}&nbsp;-&nbsp;{{item.forecastMaxtemp.value}}&#176;{{item.forecastMaxtemp.unit}}</b-card-text>
+                            <b-card-text>濕度：{{item.forecastMinrh.value}}%&nbsp;-&nbsp;{{item.forecastMaxrh.value}}%</b-card-text>
+                            
+                        </b-card>
+                    </div>
+                </b-card-group>
+            </div>
+            <p class="time">更新時間：{{$moment(Nine.updateTime).format('YYYY-MM-DD LTS')}}</p>
+            
+        </div>
+        <!-- <div style="height:2500px"></div> -->
+    </div>
+</template>
+<script lang="ts">
+import { Vue,Component,Watch } from "nuxt-property-decorator";
+import axios from 'axios';
+import moment from 'moment';
+@Component
+export default class WeatherDetail extends Vue{
+    async asyncData(){
+        console.log('This is weather_detail asyncData.');
+        let [detail,flws,raining,nine]=await Promise.all([
+        axios.get('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warningInfo&lang=tc'),
+        axios.get('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=tc'),
+        axios.get('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc'),
+        axios.get('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=tc'),
+    ])
+    return{
+            Detail:detail.data,
+            Flws:flws.data,
+            Raining:raining.data,
+            Nine:nine.data,
+        }
+    }
+}
+</script>
+<style>
+.h6{
+    font-weight: bold;
+}
+.time{
+    font-size: 80%;
+    margin-top: 0.5%;
+}
+/* .card{
+    width: 9rem;
+    width: 100%;
+    margin-right: 0.5%;
+    float: left;
+} */
+.subtitle{
+    font-weight: bold;
+    font-size: 101%;
+    color:rgb(2, 0, 139);
+}
+.detailItem{
+    margin-bottom: 0.5%;
+}
+.title{
+    font-weight: bold;
+    font-size: 120%;
+    color:darkmagenta;
+}
+.date{
+    font-size: 90%;
+}
+</style>
