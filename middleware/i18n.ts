4d9cba939f5b87d45a74f7c7790aaf56989139cf
Angus Chan <id506507@gmail.com>
2019-08-01 17:42:13 +0800

    update i18n

--- middleware/i18n.ts
+++ middleware/i18n.ts
@@ -3,9 +3,9 @@ export default function ({ isHMR, app, store, route, params, error, redirect }) 
     // If middleware is called from hot module replacement, ignore it
     if (isHMR) return
     const locale = route.query.lang || defaultLocale
-    if (store.state.locales.indexOf(locale) === -1) {
-      return error({ message: 'This page could not be found.', statusCode: 404 })
-    }
+    // if (store.state.locales.indexOf(locale) === -1) {
+    //   return error({ message: 'This page could not be found.', statusCode: 404 })
+    // }
     //Mutate the store's locale once we understand which locale is being requested prior to each page render
     store.commit('set_Lang', locale)
     // Set locale from the query string '?lang='**''
